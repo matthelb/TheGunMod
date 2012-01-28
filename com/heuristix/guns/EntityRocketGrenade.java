78d431e5aef9540712341f299235beae1ed08a91
Heuristix <heuristicks@gmail.com>
2012-01-28 16:19:33 -0500

    Update EntityRocketGrenade speed logic

--- com/heuristix/guns/EntityRocketGrenade.java
+++ com/heuristix/guns/EntityRocketGrenade.java
@@ -11,17 +11,50 @@ import net.minecraft.src.World;
  */
 public class EntityRocketGrenade extends EntityGrenade {
 
+    public static final float MAX_SPEED = 5.0f;
+    private final boolean constructed;
+
+    private float speed;
+
     public EntityRocketGrenade(World world) {
         super(world);
+        this.constructed = true;
     }
 
     public EntityRocketGrenade(World world, EntityLiving living) {
         super(world, living);
+        this.constructed = true;
+    }
+
+    public EntityRocketGrenade(World world, double x, double y, double z) {
+        super(world, x, y, z);
+        this.constructed = true;
     }
 
     @Override
     public float getMass() {
         return 0;
+    }
+
+    @Override
+    public float getSpeed() {
+        if(!constructed) {
+            return (speed = super.getSpeed());
+        }
+        return speed;
+    }
+
+    @Override
+    public void onUpdate() {
+        super.onUpdate();
+        if(speed < MAX_SPEED) {
+            if(speed == 1.0f) {
+                speed += 0.2f;
+            }
+            float newSpeed = (float) Math.min(Math.pow(speed, 1.1f), MAX_SPEED);
+            changeVelocity(newSpeed / getSpeed());
+            this.speed = newSpeed;
+        }
     }
 
 }
