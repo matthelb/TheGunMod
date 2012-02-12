0832b65fcbe3e7ee01d104a4d35bbb53d4904c6c
Heuristix <heuristicks@gmail.com>
2012-02-12 13:36:49 -0500

    Add HD Texture enabler comptibility files

---
+++ com/pclewis/mcpatcher/mod/TileSize.java
@@ -0,0 +1,11 @@
+package com.pclewis.mcpatcher.mod;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Matt
+ * Date: 2/7/12
+ * Time: 11:11 PM
+ */
+public class TileSize {
+    public static int int_size;
+}
---
+++ net/minecraft/src/Config.java
@@ -0,0 +1,18 @@
+package net.minecraft.src;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Matt
+ * Date: 2/12/12
+ * Time: 12:14 PM
+ */
+public class Config {
+
+    public static int getIconWidthTerrain() {
+        return 0;
+    }
+
+  public static int getIconWidthItems() {
+        return 0;
+  }
+}
